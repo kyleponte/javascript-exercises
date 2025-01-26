fa075a3585e08bfa326d83bdde64af9ddc562b09
Kyle <kyleponte07@gmail.com>
2025-01-26 01:05:19 -0500

    Fixed function so that it passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    stringLen = string.length;
+    reverseStr = '';
+    let i = 0;
+    while (i < stringLen) {
+        reverseStr += string[stringLen - 1 - i]
+        i++;
+    }
 
+    return reverseStr;
 };
 
 // Do not edit below this line
