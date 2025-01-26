439c7cbe714e321683c82736d1fe41f7d0458432
Kyle <kyleponte07@gmail.com>
2025-01-26 00:58:50 -0500

    Fixed function so that it passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+    finalString = '';
+
+    let i = 0
+    while (i < num) {
+        finalString += string
+        i++;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
