b198abbc0d0b40c6b57fa89cb72cd31497e1137e
Kyle <kyleponte07@gmail.com>
2025-01-26 13:19:06 -0500

    Made slight changes to make the solution more efficient

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,16 @@ const sumAll = function(start, end) {
     if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
         return 'ERROR';
     }
+
     let sum = 0
-    for (let val = Math.min(start, end); val <= Math.max(start, end); val++) {
+
+    if (start > end) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (let val = start; val <= end; val++) {
         sum += val;
     }
     return sum;
