ec3c309763f542813fc4c4aa6dbd67eee47f6242
Kyle <kyleponte07@gmail.com>
2025-01-26 13:16:40 -0500

    Made corrections to the function so that all tests are passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    let sum = 0
+    for (let val = Math.min(start, end); val <= Math.max(start, end); val++) {
+        sum += val;
+    }
+    return sum;
 };
 
 // Do not edit below this line
