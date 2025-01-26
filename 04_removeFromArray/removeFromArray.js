fdc886a756c259bf45fdfaef295aa2543395b862
Kyle <kyleponte07@gmail.com>
2025-01-26 12:53:24 -0500

    Made changes to function so that all tests are passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrValues, ...values) {
+    return arrValues.filter((value) => {
+        for (const val of values) {
+            if (value === val) {
+                return false;
+            }
+        }
+        return true;
+    })
 };
 
 // Do not edit below this line
